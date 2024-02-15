d30e1e05a0b8228bb24e791c242f928260f3c98a
HO20022 <oka.hirona1332@gmail.com>
2024-02-15 14:00:01 +0900

    use timer

---
+++ Core/Src/mrubyc/hal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "rrt0.h"
+#include "hal.h"
+#include "main.h"
+
+void hal_init(void){
+}
+
+void hal_enable_irq(void){
+	__enable_irq();
+}
+
+void hal_disable_irq(void){
+	__disable_irq();
+}
+
+void hal_idle_cpu(void){
+	/*HAL_Delay(MRBC_TICK_UNIT);
+	mrbc_tick();
+	HAL_SuspendTick();
+	HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);*/
+	HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+}
+
+extern UART_HandleTypeDef huart2;
+
+int hal_write(int fd, const void *buf, int nbytes){
+	HAL_UART_Transmit(&huart2, buf, nbytes, -1);
+	return nbytes;
+}
+
+
+
